560dbfb56b91d138e6b4f82c1496913c13d1a9df
Sunil Shah <sunil@mesosphere.io>
2015-05-01 15:52:48 -0700

    Fix desc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'package-description.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
